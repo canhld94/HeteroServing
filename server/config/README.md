e6bedb43a8fe555bdbd8055709951e2ad5e18315
canhld94 <canhld@kaist.ac.kr>
2020-09-25 10:21:45 +0900

    change parallel config file make parallel server and reactor server has same config format

--- server/config/README.md
+++ server/config/README.md
@@ -4,18 +4,42 @@ Do not add comment to the configure file
 
 ```JSON
 {
-  "ip": "IP address",
-  "port": "Port you want to run",
-  "inference engine": [
+  "ip": "your ip address",
+  "port": "your port",
+  "inference engines": [
     {
-      "name": "Name of Inference Engine",
-      "device": "Device to run, either CPU, GPU, or HETERO:FPGA,CPU",
-      "model": "path to the xml model",
-      "labels": "path to the label file",
-      "fpga configuration": {
-        "dev": "device number",
-        "bitstream": "path to the bitstream will be configured on FPGA"
-      }
+      "device": "cpu",
+      "models": [
+        {
+            "name": "name of the models",
+            "graph": "computation graph",
+            "label": "label file",
+            "replicas": "number of inference engines you want to create"
+        }
+      ]
+    },
+    {
+      "device": "fpga",
+      "models": [
+          {
+              "name": "name of the models",
+              "graph": "computation graph",
+              "label": "label file",
+              "replicas": "number of inference engines you want to create"
+          }
+      ],
+      "bitstream": "associated bitstream"
+    },
+    {
+      "device": "gpu",
+      "models": [
+          {
+              "name": "name of the models",
+              "graph": "computation graph",
+              "label": "label file",
+              "replicas": "number of inference engines you want to create"
+          }
+      ]
     }
   ]
 }
--- server/parallel/st_server_parallel.cpp
+++ server/parallel/st_server_parallel.cpp
@@ -86,7 +86,7 @@ int main(int argc, char const *argv[])
         // Set the default logger to file logger
         auto file_logger = spdlog::basic_logger_mt("basic_logger", "logs/basic.txt");
         spdlog::set_default_logger(file_logger);
-        spdlog::set_level(spdlog::level::debug);
+        spdlog::set_level(spdlog::level::info);
         spdlog::info("Log started!");
         // Check command line arguments.
         if (!parse_and_check_cmd_line(argc, const_cast<char**>(argv))) {
@@ -109,33 +109,49 @@ int main(int argc, char const *argv[])
         // server
         auto ip = config.get<std::string>("ip");
         auto port = config.get<std::string>("port");
-
+        std:: cout << ip << ":" << port << std::endl;
         // inference engine
         std::vector<inference_engine::ptr> IEs;
+
         const auto &ie_array = config.get_child("inference engines");
-        std::unordered_set<std::string> fpga_devs; 
         ie_factory factory;
+        // iterate over all devices
         for (auto it = ie_array.begin(); it != ie_array.end(); ++it) {
-            auto ie = it->second;
-            const std::string &name = ie.get<std::string>("name");
-            const std::string &device = ie.get<std::string>("device");
-            const std::string &model = ie.get<std::string>("model");
-            const std::string &labels = ie.get<std::string>("labels");
-            bool fpga = device.find("fpga") != std::string::npos;
-            if (fpga) {
-                auto &fpga_conf = ie.get_child("fpga configuration");
-                // fpga device number, cannot shared by now
-                const std::string &dev = fpga_conf.get<std::string>("dev");
-                if (fpga_devs.find(dev) != fpga_devs.end()) {
+            // get the configuration of each device
+            auto conf = it->second;
+            const std::string &device = conf.get<std::string>("device");
+            // get the models list, pass if there is no models
+            auto &model_list = conf.get_child("models");
+            if (model_list.size() == 0) continue;
+            // currently, one device can run only one models, so only get the begin
+            auto model = model_list.begin()->second;
+            const std::string &name = model.get<std::string>("name");
+            // path to the model graph and weight
+            const std::string &graph = model.get<std::string>("graph");
+            const std::string &labels = model.get<std::string>("label");
+            const int replicas = model.get<int>("replicas");
+            bool is_fpga = device.find("fpga") != std::string::npos;
+            if (is_fpga) {
+                // FPGA inference worker cannot run outside of main threads
+                // Therefore, current version of inference server can run at most
+                // one FPGA inference worker.
+                if (replicas > 1) {
                     throw fpga_overused();
                 }
-                fpga_devs.insert(dev);
                 // bitstream
-                const std::string &bitstream = fpga_conf.get<std::string>("bitstream");
+                const std::string &bitstream = conf.get<std::string>("bitstream");
                 setenv("DLA_AOCX",bitstream.c_str(),0);
                 // setenv("CL_CONTEXT_COMPILER_MODE_INTELFPGA","3",0);
             }
-            IEs.push_back(factory.create_inference_engine(name,device,model,labels));
+            // create inference engines
+            for (int i = 0; i < replicas; ++i) {
+                if (is_fpga) {
+                    IEs.insert(IEs.begin(),factory.create_inference_engine(name,device,graph,labels));
+                }
+                else {
+                    IEs.push_back(factory.create_inference_engine(name,device,graph,labels));
+                }
+            }
         }
 
         // task queue - Not necessary used with CPU inference
